126284f53b2a35be8c8e0b43e22f251b3651de5c
Greco <ojeffersonfreitas@gmail.com>
2023-01-09 11:34:29 -0400

    Badge working

--- Sources/UaiDS/Particles/Badge.swift
+++ Sources/UaiDS/Particles/Badge.swift
@@ -1,17 +1,102 @@
 import SwiftUI
 
-struct Badge: View {
+public struct Badge: View {
     
-    enum Style {
+    enum Role {
         case regular, alert, warning
     }
     
-    //var style: Style
+    var icon: String = ""
+    var label: String = ""
+    var role: Role?
     
-    var body: some View {
-        HStack {
-            Label("Label", systemImage: "uai.symbol.placeholder")
+    fileprivate func BaseBadge() -> some View {
+        Group {
+            if !icon.isEmpty && !label.isEmpty {
+                
+                HStack (spacing: 2) {
+                    
+                    Label("icon", systemImage: icon).labelStyle(.iconOnly)
+                    Text(label)
+                    
+                }
+                .uaiFont(.caption)
+                .padding(.leading, 4)
+                .padding(.trailing, 6)
+                .frame(height: 18)
+                
+            }
+            
+            if icon.isEmpty {
+                
+                Text(label)
+                    .uaiFont(.caption)
+                    .padding(.horizontal, 6)
+                    .frame(height: 18)
+                
+            }
+            
+            if label.isEmpty {
+                
+                Label("icon", systemImage: icon).labelStyle(.iconOnly)
+                    .uaiFont(.caption)
+                    .frame(width: 18, height: 18)
+                
+            }
+        }
+    }
+    
+    fileprivate func BadgeRegular() -> some View {
+        
+        BaseBadge()
+            .foregroundColor(Color.uaiAccent70)
+            .background(Color.uaiAccent20)
+            .clipShape(Capsule())
+        
+    }
+    
+    fileprivate func BadgeAlert() -> some View {
+        
+        BaseBadge()
+            .foregroundColor(Color.uaiAlert10)
+            .background(Color.uaiAlert60)
+            .clipShape(Capsule())
+        
+    }
+    
+    fileprivate func BadgeWarning() -> some View {
+        
+        BaseBadge()
+            .foregroundColor(Color.uaiWarning10)
+            .background(Color.uaiWarning60)
+            .clipShape(Capsule())
+        
+    }
+    
+    public var body: some View {
+        switch role {
+        case .regular:
+            return AnyView(BadgeRegular())
+        case .alert:
+            return AnyView(BadgeAlert())
+        case .warning:
+            return AnyView(BadgeWarning())
+        default:
+            return AnyView(BadgeRegular())
+        }
+    }
+    
+}
 
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            HStack {
+                Badge(icon: "circle.fill", label: "Label")
+                Badge(icon: "circle.fill", label: "Label", role: .alert)
+                Badge(icon: "circle.fill", label: "Label", role: .warning)
+            }
+            .padding()
         }
     }
 }
--- Sources/UaiDS/Quantums/Color/Color.swift
+++ Sources/UaiDS/Quantums/Color/Color.swift
@@ -19,4 +19,23 @@ public extension Color {
     static let uaiNeutral80 = Color("Neutral 80", bundle: .module)
     static let uaiNeutral90 = Color("Neutral 90", bundle: .module)
     
+    /// Accent
+    static let uaiAccent10 = Color("Accent 10", bundle: .module)
+    static let uaiAccent20 = Color("Accent 20", bundle: .module)
+    static let uaiAccent30 = Color("Accent 30", bundle: .module)
+    static let uaiAccent40 = Color("Accent 40", bundle: .module)
+    static let uaiAccent50 = Color("Accent 50", bundle: .module)
+    static let uaiAccent60 = Color("Accent 60", bundle: .module)
+    static let uaiAccent70 = Color("Accent 70", bundle: .module)
+    static let uaiAccent80 = Color("Accent 80", bundle: .module)
+    static let uaiAccent90 = Color("Accent 90", bundle: .module)
+    
+    /// Semantic - Alert
+    static let uaiAlert10 = Color("Alert 10", bundle: .module)
+    static let uaiAlert60 = Color("Alert 60", bundle: .module)
+    
+    /// Semantic - Warning
+    static let uaiWarning10 = Color("Warning 10", bundle: .module)
+    static let uaiWarning60 = Color("Warning 60", bundle: .module)
+    
 }

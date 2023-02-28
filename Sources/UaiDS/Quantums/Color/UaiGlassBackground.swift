cc840d4df7c36b1493c617e6c6536f0b3ada402c
Jefferson Grēco <51969359+jeffersongreco@users.noreply.github.com>
2023-02-28 14:47:08 -0400

    aaa

--- Sources/UaiDS/Quantums/Color/UaiGlassBackground.swift
+++ Sources/UaiDS/Quantums/Color/UaiGlassBackground.swift
@@ -1,6 +1,9 @@
 import SwiftUI
 
 public struct UaiGlassBackground: View {
+    
+    public init() {}
+    
     public var body: some View {
         ZStack {
             Blur(style: .systemThinMaterial)
